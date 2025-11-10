41959d92c01d620aff5c86c06c3f978603ab0c0c
Pablo Djezzy <daiayoub59@gmail.com>
2025-11-10 10:52:58 +0100

    Remove deprecated Next.js configuration files and update output settings in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
 };
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
+
+module.exports = {
+  output: 'export',
+}
